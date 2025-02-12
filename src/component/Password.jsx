395f651bc979c3d7316878428c043a9f4f80e6d3
Erfan <erfan.taheri.4350@gmail.com>
2025-02-12 11:52:38 +0330

    Start to style and install a extesion for Tailwind

--- src/component/Password.jsx
+++ src/component/Password.jsx
@@ -3,12 +3,14 @@ export function Password(){
     const [password , setPassword ] =  useState("");
         return(
         <>
+        <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
         <h1>Please enter correct password</h1>
         <form action="#" method="post">
         <label htmlFor="password">Pssword</label>
         <input type="password" name="password" id="password" />
-         <button>Send</button>
+         <div><button>Send</button></div>
         </form>
+        </div>
         </>
     )
 }
