8ee016028680800d017affc27f5a93cfb50d346e
Erfan <erfan.taheri.4350@gmail.com>
2025-02-12 15:22:46 +0330

    Cange styles

--- src/component/Password.jsx
+++ src/component/Password.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react"
-export function Password(){
-    const [password , setPassword ] =  useState("");
-        return(
-        <>
-        <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
-        <h1>Please enter correct password</h1>
-        <form action="#" method="post">
-        <label htmlFor="password">Pssword</label>
-        <input type="password" name="password" id="password" />
-         <div><button>Send</button></div>
+import { useState } from "react";
+export function Password() {
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center ">
+      <div className="rounded-md bg-primary-color p-10 m-8 border border-solid border-indigo-600 ">
+        <h1 className=" text-Text-color text-lg font-bold font  p-8 m-1">
+          Please enter correct password
+        </h1>
+        <form className="" action="#" method="post">
+          <label
+            className="ml-1.5 w-[50%] text-Text-color p-8 "
+            htmlFor="password"
+          >
+            Pssword:
+          </label>
+          <input
+            className="bg-Secondary-color w-[48%] text-Text-color p-3  focus:outline-accent-color-1"
+            type="password"
+            name="password"
+            id="password"
+          />
+          <div className="flex justify-center">
+            <button className="border w-[50%]  text-Text-color  border-accent-color-1 py-2 px-4 mt-8">
+              Send
+            </button>
+          </div>
         </form>
-        </div>
-        </>
-    )
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#4A90E2",
+        "Secondary-color": "#D5E1DF",
+        "accent-color-1": "#F5A623", 
+        "accent-color-2": "#7ED321",
+        "Text-color": "#333333" ,
+      },
+    },
   },
   plugins: [],
-}
-
+};
