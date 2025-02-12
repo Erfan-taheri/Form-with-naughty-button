6b8a359b3123d107d92208454dbd581e0ece7040
Erfan <erfan.taheri.4350@gmail.com>
2025-02-12 11:28:48 +0330

    create related form to our purpose

---
+++ src/component/Password.jsx
@@ -0,0 +1,11 @@
+export function Password(){
+    return(
+        <>
+        <h1>Please enter correct password</h1>
+        <form action="#" method="post">
+        
+        
+        </form>
+        </>
+    )
+}
