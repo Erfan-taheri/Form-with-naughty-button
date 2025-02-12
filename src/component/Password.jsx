9d30b2606ca334dd7489e267c45bdb1568034e98
Erfan <erfan.taheri.4350@gmail.com>
2025-02-12 15:28:27 +0330

    Catch value of password input

--- src/component/Password.jsx
+++ src/component/Password.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 export function Password() {
   const [password, setPassword] = useState("");
-
+function handlePassword(event){
+    console.log(password)
+    setPassword(event.target.value)
+}
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center ">
-      <div className="rounded-md bg-primary-color p-10 m-8 border border-solid border-indigo-600 ">
+      <div className="rounded-md bg-primary-color p-10 m-8 border border-solid border-accent-color-1 ">
         <h1 className=" text-Text-color text-lg font-bold font  p-8 m-1">
           Please enter correct password
         </h1>
@@ -16,6 +19,7 @@ export function Password() {
             Pssword:
           </label>
           <input
+          onChange={handlePassword}
             className="bg-Secondary-color w-[48%] text-Text-color p-3  focus:outline-accent-color-1"
             type="password"
             name="password"
